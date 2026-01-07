4c3743b51bb4ca8b2102d9cdefaa66a850fae064
Kira Klingenberg <kirak@pdx.edu>
2026-01-07 12:56:10 -0800

    Added a main calling routine for binary search functions

--- Searches/binary.py
+++ Searches/binary.py
@@ -32,6 +32,21 @@ def binary_recursive(sorted, target, start, end):
         return binary_recursive(sorted, target, start, mid-1)
         #throws out the right recurses to the left
     return -1
+
+def main():
+    array = [8,9,10,11,12]
+    target = 12 #should be found at index 4
+    target_nope = 100 #should not be found
+    size = len(array)
+    result = binary_search(array, target, size)
+    #Uncomment out the line below, and comment out the line above to run the recursive version
+    #result = binary_wrapper(array, size, target)
+    if result > 0:
+        print(f"The target was found at index {result}")
+    else:
+        print("The target was not found.")
+
+main()
     
 
 
