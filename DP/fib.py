320e77f04dc97903507071ca7f78fe90f69e309a
Kira Klingenberg <kirak@pdx.edu>
2026-01-26 13:23:11 -0800

    Adding naiive recursive and Memoized code for the fibnacci problem

---
+++ DP/fib.py
@@ -0,0 +1,30 @@
+#naiive recursive
+# Big O (2^n)
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+    ans = fib(n-1) + fib (n-2)
+    return ans
+
+# Memoized version
+# Big O (n)
+def fib_memo(n, array):
+    #Is this n already solved?
+    if array[n] != -1:
+        return array[n]
+    #if not, solve it
+    ans = fib_memo(n-1, array) + fib_memo(n-1, array)
+    array[n] = ans #add my answer to array
+    return ans #give my answer to my parent
+
+def main():
+    n = int(input())
+    array = [-1] * (n+1) #generate soluton array
+    #populate known base case solution values
+    array[0] = 0
+    array[1] = 1
+    
+    ans = fib_memo(n, array)
+    print(f"The {n} fibonacci number is: {ans}")
+
+main()
